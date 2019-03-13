4b97bfe0214a7e02bcb8c234486d7c919010d143
JimmyLIn <linyuan5856139@163.com>
2019-03-14 01:07:04 +0800

    chapter2 update

--- CPP_Primer/Chapter2.cpp
+++ CPP_Primer/Chapter2.cpp
@@ -22,5 +22,35 @@ int main()
     }
 
     std::cout << index << std::endl;
+
+    //引用（引用不能为空 必须初始化）
+
+    int value = 2048;
+    int &refVal = value;
+    refVal = 1; //value的值 会呗更改
+
+    std::cout << value << std::endl;
+
+    //指针（可以不初始化 为nullptl）
+    int val = 1024;
+    int *p = &val;
+    std::cout << val << std::endl;
+    std::cout << p << std::endl;
+    std::cout << *p << std::endl;
+
+    //void* 任意对象的地址
+    double obj = 3.1415926;
+    double *pd = &obj;
+
+    void *pv = &obj;
+    if (pv == pd)
+    {
+        std::cout << "pv == pd " << std::endl;
+    }
+
+    double *test = static_cast<double *>(pv);
+
+    std::cout << *test << std::endl;
+
     return 0;
 }
